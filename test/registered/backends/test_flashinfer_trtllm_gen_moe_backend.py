30a139640011afdd4bfda4c7522e0b296bd7df0f
Ziang Li <ziangli@umich.edu>
2026-05-19 01:04:48 -0700

    [FlashInfer v0.6.11] [RL] Support FlashInfer per-token NVFP4 MoE (#22918)

--- test/registered/backends/test_flashinfer_trtllm_gen_moe_backend.py
+++ test/registered/backends/test_flashinfer_trtllm_gen_moe_backend.py
@@ -159,6 +159,7 @@ class FlashinferTrtllmGenMoeBackendMXFP8Base:
 
 class FlashinferTrtllmGenMoeBackendNVFP4Base:
     backend = None
+    extra_env = {}
 
     @classmethod
     def setUpClass(cls):
@@ -168,7 +169,7 @@ class FlashinferTrtllmGenMoeBackendNVFP4Base:
             cls.model,
             cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            env={**os.environ, "SGLANG_ENABLE_JIT_DEEPGEMM": "False"},
+            env={**os.environ, **cls.extra_env, "SGLANG_ENABLE_JIT_DEEPGEMM": "False"},
             other_args=[
                 "--moe-runner-backend",
                 cls.backend,
@@ -206,28 +207,10 @@ class TestFlashinferTrtllmGenMoeBackendFP8(
     backend = "flashinfer_trtllm"
 
 
-class TestFlashinferTrtllmGenMoeBackendMXFP8(
-    FlashinferTrtllmGenMoeBackendMXFP8Base, CustomTestCase
-):
-    backend = "flashinfer_trtllm"
-
-
-class TestFlashinferTrtllmGenMoeBackendBF16(
-    FlashinferTrtllmGenMoeBackendBF16Base, CustomTestCase
-):
-    backend = "flashinfer_trtllm"
-
-
 class TestFlashinferTrtllmGenMoeBackendNVFP4(
     FlashinferTrtllmGenMoeBackendNVFP4Base, CustomTestCase
 ):
     backend = "flashinfer_trtllm"
-
-
-class TestFlashinferTrtllmGenMoeBackendFP8Routed(
-    FlashinferTrtllmGenMoeBackendFP8Base, CustomTestCase
-):
-    backend = "flashinfer_trtllm_routed"
 
 
 class TestFlashinferTrtllmGenMoeBackendMXFP8Routed(
@@ -242,9 +225,10 @@ class TestFlashinferTrtllmGenMoeBackendBF16Routed(
     backend = "flashinfer_trtllm_routed"
 
 
-class TestFlashinferTrtllmGenMoeBackendNVFP4Routed(
+class TestFlashinferTrtllmGenMoeBackendPerTokenNVFP4Routed(
     FlashinferTrtllmGenMoeBackendNVFP4Base, CustomTestCase
 ):
+    extra_env = {"SGLANG_FLASHINFER_NVFP4_PER_TOKEN_ACTIVATION": "1"}
     backend = "flashinfer_trtllm_routed"
 
 
